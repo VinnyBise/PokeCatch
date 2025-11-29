293cee6ef1d6d879ea20220bdc97086b8ab0049f
Bontuyan <jabbontuyan03202400138@usep.edu.ph>
2025-11-29 23:34:13 +0800

    shiny magicarp

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("marjhun gaming");
         // task 1 marjhun pamilar 
         // task 2 Charmander Shiny
-        // task 3
+        // task 3 Shiny Magicarp
         // task 4
     }
 }
