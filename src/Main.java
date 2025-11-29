4180ad43e320645886da819a168a94f3c8642bd6
Bontuyan <jabbontuyan03202400138@usep.edu.ph>
2025-11-29 23:29:02 +0800

    added charmander shiny

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Welcome to PokeCatch!");
         System.out.println("marjhun gaming");
         // task 1 marjhun pamilar 
-        // task 2
+        // task 2 Charmander Shiny
         // task 3
         // task 4
     }
