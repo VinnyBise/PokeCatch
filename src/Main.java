63ffa17ba7bef0b7b6df1fd692821e45c97267a7
Bontuyan <jabbontuyan03202400138@usep.edu.ph>
2025-11-29 23:13:57 +0800

    added introduction line at Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to PokeCatch!");
+        System.out.println("We will catch alot of Pokemon!");
         // task 1 marjhun pamilar
-
         // task 2
         // task 3
         // task 4
