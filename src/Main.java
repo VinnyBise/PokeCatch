98048738d0a61bd037f2e4a8018bc94b6220e343
Bontuyan <jabbontuyan03202400138@usep.edu.ph>
2025-11-29 23:04:54 +0800

    added marjhun at Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to PokeCatch!");
-        System.out.println("Marjhun");
-        System.out.println("Marjhun");
-        System.out.println("Marjhun");
-        System.out.println("Marjhun");
-        
-    
-    
-        // task 1 
+        // task 1 marjhun
         // task 2
         // task 3
         // task 4
